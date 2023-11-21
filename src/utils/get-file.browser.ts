b46469cad75a9b6252426cfed8cc06de60fe0f85
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-11-21 02:23:29 +0000

    refactor: simplify

--- src/utils/get-file.browser.ts
+++ src/utils/get-file.browser.ts
@@ -1,5 +1,4 @@
-import { isntString } from '@blackglory/prelude'
-import { assert } from '@blackglory/prelude'
+import { isntString, assert } from '@blackglory/prelude'
 
 export async function getFile(blob: Blob | string): Promise<Blob> {
   assert(isntString(blob), 'The function only accepts Blob on browser side')
